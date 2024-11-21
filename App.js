8522da1092bf856be1653e4e374537c1fa444f52
tamoorhaseeb <tamoor.haseeb@devexcelit.com>
2024-11-21 10:04:18 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement("div", { id: "child" }, [
+      React.createElement("h1", { id: "h1tag" }, "I'm an h1 tag"),
+      React.createElement("h1", { id: "h2tag" }, "I'm an h2 tag"),
+    ])
+  );
+  const heading = React.createElement(
+    "h1",
+    { id: "heading" },
+    "Hello World from React"
+  );
+  console.log(parent);
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(parent);
+});
